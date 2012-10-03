d28eb454e205fcf3f22016f87a6796bee1cf421f
darki73 <dev@aftersoft.ru>
2012-10-03 01:52:43 +0100

    Version Manager for DataBase (WIP)

---
+++ SQL/freedomhead_install.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `aowow_version` (
+  `version` mediumint(11) unsigned NOT NULL,
+  `sql_version` varchar(255) NOT NULL,
+  `revision` varchar(255) NOT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+INSERT INTO `aowow_version` (`version`, `sql_version`, `revision`) VALUES ('1', '1', '1');
